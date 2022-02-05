a59b967fb67125e7dc68aa707bdfc0398833f618
Wisnu Pratama <wisnuputrapratama24@gmail.com>
2022-02-05 15:32:36 +0700

    navbar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Logo } from "..";
 import styles from "./Navbar.module.scss";
 
@@ -31,13 +31,50 @@ const navItem = [
 ];
 
 function Navbar() {
-  return (
-    <header>
-      <div className={`px-8 ${styles.navbar}`}>
-        <Logo />
-        <NavLink />
+  const [showMenu, setShowMenu] = useState<Boolean>(false);
+  const [screenWidth, setScreenWidth] = useState<Number>(() => {
+    if (typeof window !== "undefined") {
+      return window.innerWidth;
+    } else {
+      return 380;
+    }
+  });
 
-        <div>
+  const handleClickHamburger = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
+  return (
+    <header className={styles.navbarRoot}>
+      <div className={`px-8 max-w-screen-xl mx-auto ${styles.navbar}`}>
+        <Logo />
+        <div
+          className='md:hidden block cursor-pointer'
+          onClick={handleClickHamburger}>
+          <HamburgerIcon />
+        </div>
+        <NavLink
+          showMenu={showMenu}
+          screenWidth={screenWidth}
+          handleClickHamburger={handleClickHamburger}
+        />
+
+        <div className='hidden lg:flex items-center font-bold text-lg'>
+          <Link href='/register'>
+            <a className='text-white mr-5'>REGISTER</a>
+          </Link>
           <Button type='button' label='LOGIN' />
         </div>
       </div>
@@ -45,21 +82,81 @@ function Navbar() {
   );
 }
 
-const NavLink = () => {
+interface NavLinkProps {
+  showMenu: Boolean;
+  screenWidth: Number;
+  handleClickHamburger: Function;
+}
+const NavLink = ({
+  showMenu,
+  screenWidth,
+  handleClickHamburger,
+}: NavLinkProps) => {
   return (
-    <div className={styles.nav}>
+    <div
+      className={`hidden lg:flex ${styles.nav} ${
+        showMenu && screenWidth <= 768 ? styles.show : ""
+      }`}
+      id='nav-item'>
+      {showMenu && screenWidth <= 768 && (
+        <div
+          className='absolute right-5 mt-5 cursor-pointer'
+          onClick={() => handleClickHamburger()}>
+          <SilangIcon />
+        </div>
+      )}
+
       <ul>
         {navItem.map((row, index) => {
           return (
             <li key={index}>
-              <Link href={row.link}>
-                <a> {row.name}</a>
-              </Link>
+              <Link href={row.link}>{row.name}</Link>
             </li>
           );
         })}
       </ul>
+
+      {showMenu && screenWidth <= 768 && (
+        <div className='mx-auto flex justify-center'>
+          <Button type='button' label='LOGIN' />
+        </div>
+      )}
     </div>
+  );
+};
+
+const SilangIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6 text-xl text-white font-bold'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M6 18L18 6M6 6l12 12'
+      />
+    </svg>
+  );
+};
+const HamburgerIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6 text-gray-500'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M4 6h16M4 12h16M4 18h16'
+      />
+    </svg>
   );
 };
 
